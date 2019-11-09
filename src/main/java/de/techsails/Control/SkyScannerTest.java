a3e93109cdf1b399f62a62c77f932b7674c85508
Mathew Allington <funkymallington@gmail.com>
2019-11-09 21:57:09 +0100

    Changes

--- src/main/java/de/techsails/Control/SkyScannerTest.java
+++ src/main/java/de/techsails/Control/SkyScannerTest.java
@@ -3,15 +3,27 @@ package de.techsails.Control;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import de.techsails.Control.SkyScanner.FlightPreference;
 import de.techsails.JavaJSON.JSONException;
 
 public class SkyScannerTest {
-    public static String SKY_SCANNER_KEY = "dfdsf";
+    public static String SKY_SCANNER_KEY = "jacobs-2019";
     public static void main(String[] args) throws MalformedURLException, JSONException, IOException{
 
         SkyScanner travel = new SkyScanner(SKY_SCANNER_KEY);
-        travel.connect();
-        System.out.println(travel.getSecret());
+        try {
+        	 System.out.println(travel.getPlaces(""));
+        	 
+			travel.getFlights("MANC-sky", "LOND-sky", FlightPreference.CHEAPEST);
+			
+			
+			 
+		} catch (Exception e) {
+			System.out.println("flight crashed");
+			e.printStackTrace();
+		}
+        
+       
        
     }
 }
