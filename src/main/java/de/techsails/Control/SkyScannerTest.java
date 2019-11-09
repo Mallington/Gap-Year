48a146a0595d3e4349ddabd11f4aa7a76082a9d7
Mathew Allington <funkymallington@gmail.com>
2019-11-09 18:07:54 +0100

    Working class

--- src/main/java/de/techsails/Control/SkyScannerTest.java
+++ src/main/java/de/techsails/Control/SkyScannerTest.java
@@ -8,12 +8,11 @@ import de.techsails.JavaJSON.JSONException;
 public class SkyScannerTest {
     public static String SKY_SCANNER_KEY = "skyscanner-guts2019";
     public static void main(String[] args) throws MalformedURLException, JSONException, IOException{
-        Request request = new Request();
 
-        TravelCommon travel = new TravelCommon(SKY_SCANNER_KEY, request);
+        SkyScanner travel = new SkyScanner(SKY_SCANNER_KEY);
+        travel.
 
-        
-            System.out.println(travel.getLocales());
+      
         
     }
 }
