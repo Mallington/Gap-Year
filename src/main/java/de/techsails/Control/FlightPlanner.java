4f95b9a79baaa1f31c33d6d0b4f94b4894835fbe
Mekacher_Anis <haithem1297@outlook.fr>
2019-11-09 17:59:48 +0100

    e

--- src/main/java/de/techsails/Control/FlightPlanner.java
+++ src/main/java/de/techsails/Control/FlightPlanner.java
@@ -7,5 +7,10 @@ public class FlightPlanner {
 	public List<Flight> getFilghtPlan(List<String> contries) {
 		return null;
 	}
+}
 
+
+class geoCodes{
+	public double longitude;
+	public double latitude;
 }
