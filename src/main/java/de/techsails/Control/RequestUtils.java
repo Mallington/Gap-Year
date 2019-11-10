a0a44387be3a573bafb58fd5e7980367f4ddd291
Mathew Allington <funkymallington@gmail.com>
2019-11-10 09:34:43 +0100

    Cacheing system

--- src/main/java/de/techsails/Control/RequestUtils.java
+++ src/main/java/de/techsails/Control/RequestUtils.java
@@ -1,13 +1,95 @@
 package de.techsails.Control;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import de.techsails.JavaJSON.JSONObject;
 
 public class RequestUtils {
+	public static String CACHE_LOCATION = "cached.c";
+	private static RequestUtils cached = null;
+	
+	private static RequestUtils getCache() {
+		return (cached==null)? load() : cached;
+	}
+	
+	private HashMap<String, String> map;
+	
+	private RequestUtils() {map = new HashMap<String,String>();}
+	
+	public static void save() {
+		try {
+		JSONObject obj = new JSONObject(getCache().map);
+		writeAllText(CACHE_LOCATION,obj.toString());
+		}
+		catch(Exception e) {
+			System.out.println("Caching Failed");
+		}
+		
+		//Files.writeStringToFile(new File("cache.c"), obj.toString());
+	}
+	
+	public static RequestUtils load(){
+		
+		RequestUtils reqUtil = new RequestUtils();
+		try {
+		String JSON = readAllText(CACHE_LOCATION);
+		JSONObject jsonMap = new JSONObject(JSON);
+		
+		 Iterator<String> iterator = jsonMap.keys();
+		
+		while(iterator.hasNext()) {
+			String key = iterator.next();
+			reqUtil.map.put(key, jsonMap.getString(key));
+			System.out.println(key);
+		}
+		}
+		catch(Exception e) {
+			System.out.println("Failed to parse");
+			e.printStackTrace();
+		}
+		return (cached = reqUtil);
+	}
+	
+	
+	
+	private static void writeAllText(String path, String text) throws IOException {
+		File file = new File(path);
+		FileWriter writer = new FileWriter(file);
+		writer.write(text);
+		writer.close();
+		System.out.println("Write: "+text);
+	}
+	
+	private static String readAllText(String path) throws IOException {
+		BufferedReader bf = new BufferedReader(new FileReader(path));
+		
+		StringBuilder built = new StringBuilder();
+		built.append(bf.readLine());
+		bf.close();
+		System.out.println(built.toString());
+		return built.toString();
+	}
+	
+	
 	public static String sendGet(String url, String apiKey) throws Exception {
-		System.out.println("GET "+url);
+		
+		RequestUtils req = getCache();
+		
+		if(!req.map.containsKey(url)) {
+			
         HttpURLConnection httpClient =
                 (HttpURLConnection) new URL(url).openConnection();
 
@@ -39,8 +121,13 @@ public class RequestUtils {
             in.close();
             //print result
 
+            req.map.put(url,response.toString());
+            req.save();
            return response.toString();
-
+        }
+        }
+		else {
+        	return req.map.get(url);
         }
 
     }
