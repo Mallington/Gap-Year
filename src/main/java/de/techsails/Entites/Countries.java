4f33e7c28c571a9d1e7e9771e3f17ea01d7576e9
yousif94 <yousif_amanuel@yahoo.com>
2019-11-10 04:03:20 +0100

    countries upload added

---
+++ src/main/java/de/techsails/Entites/Countries.java
@@ -0,0 +1,24 @@
+package de.techsails.Entites;
+
+import java.util.List;
+
+public class Countries {
+
+	List<String> countries;
+
+	public Countries() {
+	}
+
+	public Countries(List<String> countries) {
+		this.countries = countries;
+	}
+
+	public List<String> getCountries() {
+		return countries;
+	}
+
+	public void setCountries(List<String> countries) {
+		this.countries = countries;
+	}
+
+}
